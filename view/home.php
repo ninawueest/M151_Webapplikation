b6bd190b44ca5c515c677e9ad867fdcf62305165
ThaNaToX <c0mm4nd3rb13st@gmail.com>
2018-12-21 13:25:47 +0100

    added navbar and footer to home.php

--- view/home.php
+++ view/home.php
@@ -3,12 +3,32 @@
 <head>
     <title>Login</title>
     <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 </head>
 
 <body>
-    <button>
-        Logout
-    </button>
+
+    <!---navbar--->
+    <nav class="navbar navbar-dark bg-dark">
+        <!-- Navbar content -->
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">Modul 151 Web-Application</a>
+            </div>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="index.html" style="color:white">Logout</a></li>
+            </ul>
+        </div>
+    </nav>
+
 </body>
+
+<!-- Footer -->
+<footer class="page-footer font-small bg-dark footer">
+    <!-- Content -->
+    <div class="footer-copyright text-center py-3">
+        Nina Wüest | Dominik Rimann
+    </div>
+</footer>
