7d507eb355eb30521955e78996203fea5698e706
ThaNaToX <c0mm4nd3rb13st@gmail.com>
2019-01-22 15:02:37 +0100

    added google maps js api

--- view/home-view.php
+++ view/home-view.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php require_once "../php/db_connection.php"; ?>
     <?php require_once "../php/functions.php"; ?>
+    <script language="javascript" type="text/javascript" src="../js/googlemaps.js"></script>
 
 </head>
 
@@ -21,6 +22,7 @@
                 <a class="navbar-brand" href="#">Modul 151 Web-Application</a>
             </div>
             <ul class="nav navbar-nav navbar-right">
+                <!---logout link--->
                 <li><a href="../php/logout.php" style="color:white">Logout</a></li>
             </ul>
         </div>
@@ -31,6 +33,11 @@
             <!--return the username session-->
             <?php return_username(); ?>
     </span>
+
+    <!---google maps js api--->
+    <div id="googleMap" style="width:100%;height:400px;margin-top:800px;margin-bottom:100px"></div>
+    <script>function myMap();</script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAXFCmrTMaJjS6p5dQmPKasqMEm_G70wuc&callback=myMap"></script>
 
 </body>
 
